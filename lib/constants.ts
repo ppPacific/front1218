8b1977e6dc0f224c56c820ffd518fd0e45391a1d
pppacific <hohohorlick259@gmail.com>
2026-03-17 23:36:51 +0800

    modified event data

--- lib/constants.ts
+++ lib/constants.ts
@@ -9,6 +9,6 @@ export type EventItem = {
 
 export const events: EventItem[] = [
     {image:'/images/img_adoptbanner.jpg',title:'Adopt 1',slug:'event-dog-1',location:'event-dog-1',date:'Date-1',time:'Time-1'},
-    {image:'/images/img_octdog.jpg',title:'Adopt 2',slug:'event-dog-1',location:'event-dog-1',date:'Date-1',time:'Time-1'},
-
+    {image:'/images/img_octdog.jpg',title:'Adopt 2',slug:'event-dog-2',location:'event-dog-1',date:'Date-1',time:'Time-1'},
+    {image:'/images/img_octdog.jpg',title:'Adopt 3',slug:'event-dog-3',location:'event-dog-1',date:'Date-1',time:'Time-1'},
 ]
