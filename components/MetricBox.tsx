d7a7c7d9529b63afa5232414bff410002c5f5045
pppacific <hohohorlick259@gmail.com>
2026-04-02 23:56:00 +0800

    update

--- components/MetricBox.tsx
+++ components/MetricBox.tsx
@@ -13,7 +13,7 @@ interface Props {
 const MetricBox = ({ title, image, slug, location, date, time }: Props) => {
     return (
         <Link href={`/events/${slug}`} id="metric-card">
-            <Image src={image} alt={title} width={410} height={300} className="poster" />
+            {/*<Image src={image} alt={title} width={410} height={300} className="poster" />*/}
 
             <div className="flex flex-row gap-2">
                 <Image src="/icons/pin.svg" alt="location" width={14} height={14} />
