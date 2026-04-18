43b01c491815ac36febb4432e02266e4fa77e847
pppacific <hohohorlick259@gmail.com>
2026-04-18 23:14:56 +0800

    add search input

--- components/DogSearchModal.tsx
+++ components/DogSearchModal.tsx
@@ -2,6 +2,7 @@
 import { X } from "lucide-react";
 import Image from "next/image";
 import React from "react";
+import {Button} from "@/components/ui/button";
 
 type DogSearchModalProps = {
     open: boolean;
@@ -10,7 +11,7 @@ type DogSearchModalProps = {
 const DogSearchModal = ({ open, onClose }: DogSearchModalProps) => {
     if (!open) return null;
     return (
-        <div className={"min-h-[calc(100dvh-12rem)] max-h-[calc(100dvh-4rem)] backdrop-blur-sm inset-0 text-black fixed top-0 left-0 w-full flex flex-col z-60 overflow-y-auto bg-white"}>
+        <div className={"px-4 min-h-[calc(100dvh-12rem)] max-h-[calc(100dvh-4rem)] backdrop-blur-sm inset-0 text-black fixed top-0 left-0 w-full flex flex-col z-60 overflow-y-auto bg-white"}>
             <div className={"relative"}>
                 <div
                     className={'absolute right-[8%] mt-10 cursor-pointer'}>
@@ -18,8 +19,8 @@ const DogSearchModal = ({ open, onClose }: DogSearchModalProps) => {
                         <X className={"h-4 w-4 font-semibold"}/>
                     </span>
                 </div>
-            </div>
-                <div className="mt-12">
+                </div>
+                <div className="mt-4">
                     <Image
                         src={"/images/logo_bringlovehome_b.png"}
                         alt={"Bring Love Home"}
@@ -30,13 +31,16 @@ const DogSearchModal = ({ open, onClose }: DogSearchModalProps) => {
                     />
                 </div>
 
-            <div className={"h-[200px]"}>
-                <p>header</p>
-            </div>
-            <div>dog search content</div>
-            <div>dog search content</div>
-            <div>dog search content</div>
-            <div>dog search content</div>
+                <div className={"modal__search-wrap"}>
+                    <div className={"modal__search"}>
+                        <div className={"modal__input-wrap"}>
+                            <input className={"modal__input"} placeholder={"search doggies..."} value={""} type={"text"} onChange={()=>{}}/>
+                        </div>
+                        <Button className={"modal__button"}>SEARCH</Button>
+                        </div>
+
+                </div>
+                <div className={"suggestion_block"}>category</div>
         </div>
     )
 }
