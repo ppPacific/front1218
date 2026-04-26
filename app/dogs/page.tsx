ef45b97fbed513075551514b999ec88c8825a6df
pppacific <hohohorlick259@gmail.com>
2026-04-26 21:49:53 +0800

    fix del useless page

--- app/dogs/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Suspense } from "react";
-
-export default function Page() {
-  return (
-    <Suspense fallback={null}>
-      <h1>Hello, General Dog Page!</h1>
-    </Suspense>
-  );
-}
--- app/events/page.tsx
+++
@@ -1,5 +0,0 @@
-
-
-export default function EventPage() {
-    return <h1>Hello, Event Page!</h1>
-}
