e28ab6ff2cc721676881121a18010ac305f91844
pppacific <hohohorlick259@gmail.com>
2026-04-26 22:03:54 +0800

    fix deploy navbar

---
+++ app/not-found/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div className={`flex justify-center align-middle mx-auto`}>
+      <h2>Your requested page not found or not authorized, please go back.</h2>
+    </div>
+  );
+};
+export default Page;
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,73 +1,80 @@
-"use client"
-import React, {useState} from 'react'
-import {Button} from "@/components/ui/button";
-import {ModeToggle} from "@/components/ModeToggle";
-import {FolderLike} from "@/components/FolderLike";
-import {SignedIn, SignedOut, SignInButton, SignUpButton, UserButton} from "@clerk/nextjs";
-import {currentUser} from "@clerk/nextjs/server";
-import { Search } from 'lucide-react';
+"use client";
+import React, { Suspense, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ModeToggle";
+import { FolderLike } from "@/components/FolderLike";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
+import { Search } from "lucide-react";
 import DogSearchModal from "@/components/DogSearchModal";
-import {usePathname, useRouter} from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 const NavBar = () => {
-    // const user = await currentUser()
-    const [open, setOpen] = useState(false);
-    const pathname = usePathname();
-    const router = useRouter();
+  // const user = await currentUser()
+  const [open, setOpen] = useState(false);
+  const pathname = usePathname();
+  const router = useRouter();
 
-    const handleSearchClick = () => {
-        if (pathname && pathname.includes("/search-result")) {
-            const existingSearchInput = document.getElementById("dogs-search-input");
-            if (existingSearchInput) {
-                //existingSearchInput.focus();
-                existingSearchInput.scrollIntoView({
-                    behavior: "smooth",
-                    block: "center",
-                });
-            } else {
-                router.push("/search-result");
-            }
-            return;
-        }
+  const handleSearchClick = () => {
+    if (pathname && pathname.includes("/search-result")) {
+      const existingSearchInput = document.getElementById("dogs-search-input");
+      if (existingSearchInput) {
+        //existingSearchInput.focus();
+        existingSearchInput.scrollIntoView({
+          behavior: "smooth",
+          block: "center",
+        });
+      } else {
+        router.push("/search-result");
+      }
+      return;
+    }
 
-        setOpen(true);
-    };
-    //console.log(open)
-    return (
-        <div className={`flex items-center justify-end gap-x-2`}>
-            <SignedOut>
-                <SignInButton mode={"modal"}>
-                    <Button
-                        variant={'outline'}
-                        className="shadow-md text-xs sm:text-sm sm:h-10 px-2 sm:px-4 cursor-pointer">
-                        Sign In
-                    </Button>
-                </SignInButton>
-                <SignUpButton>
-                    <Button className="bg-gradient-to-r from-green-400 to-blue-500 text-xs sm:text-sm sm:h-10 px-2 sm:px-4 cursor-pointer">
-                        Sign Up
-                    </Button>
-                </SignUpButton>
-            </SignedOut>
-            <SignedIn>
-                <UserButton />
-            </SignedIn>
+    setOpen(true);
+  };
+
+  return (
+    <Suspense fallback={null}>
+      <div className={`flex items-center justify-end gap-x-2`}>
+        <SignedOut>
+          <SignInButton mode={"modal"}>
             <Button
-                variant={"outline"}
-                aria-label={"Open Search"}
-                onClick={() => {
-                    setOpen(true);
-                    handleSearchClick
-                }}>
-                <Search />
+              variant={"outline"}
+              className="shadow-md text-xs sm:text-sm sm:h-10 px-2 sm:px-4 cursor-pointer"
+            >
+              Sign In
             </Button>
+          </SignInButton>
+          <SignUpButton>
+            <Button className="bg-gradient-to-r from-green-400 to-blue-500 text-xs sm:text-sm sm:h-10 px-2 sm:px-4 cursor-pointer">
+              Sign Up
+            </Button>
+          </SignUpButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <Button
+          variant={"outline"}
+          aria-label={"Open Search"}
+          onClick={() => {
+            setOpen(true);
+            handleSearchClick;
+          }}
+        >
+          <Search />
+        </Button>
 
-            {/*<ModeToggle />*/}
-            <FolderLike />
-            {
-                open && <DogSearchModal open={open} onClose={()=>setOpen(false)}/>
-
-            }
-        </div>
-    )
-}
-export default NavBar
+        {/*<ModeToggle />*/}
+        <FolderLike />
+        {open && <DogSearchModal open={open} onClose={() => setOpen(false)} />}
+      </div>
+    </Suspense>
+  );
+};
+export default NavBar;
