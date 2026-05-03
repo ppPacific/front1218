be4398f0a2a6d3e5495575c3e577be69cbef1675
pppacific <hohohorlick259@gmail.com>
2026-05-03 20:00:08 +0800

    update result pg

---
+++ components/SearchResults.tsx
@@ -0,0 +1,11 @@
+export default async function ResultDetails({
+  query,
+  currentPage,
+}: {
+  query: string;
+  currentPage: number;
+}) {
+  //const invoices = await fetchFilteredInvoices(query, currentPage);
+  // ...
+  console.log(`wait results`);
+}
