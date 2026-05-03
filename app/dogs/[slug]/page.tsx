5e5d571d27a6796b58d2c5830186dc00dc2c920a
pppacific <hohohorlick259@gmail.com>
2026-05-03 18:34:34 +0800

    skele in dog page

--- app/dogs/[slug]/page.tsx
+++ app/dogs/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import React, { Suspense } from "react";
 import DogDetails from "@/components/DogDetails";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const DogDetailPage = async ({
   params,
@@ -8,7 +9,15 @@ const DogDetailPage = async ({
 }) => {
   const slug = params.then((p) => p.slug);
   return (
-    <Suspense fallback={<div>Loading..</div>}>
+    <Suspense
+      fallback={
+        <div className="flex w-full max-w-lg flex-col gap-2">
+          <Skeleton className="h-4 w-full" />
+          <Skeleton className="h-4 w-full" />
+          <Skeleton className="h-4 w-3/4" />
+        </div>
+      }
+    >
       <DogDetails params={slug} />
     </Suspense>
   );
