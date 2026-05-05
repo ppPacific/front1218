596f3b006ec1edcaa13589dc537f15f47801ed8a
pppacific <hohohorlick259@gmail.com>
2026-05-06 01:02:25 +0800

    add logo

--- app/donation/page.tsx
+++ app/donation/page.tsx
@@ -1,11 +1,22 @@
 import React, { Suspense } from "react";
+import Image from "next/image";
 
 const Page = () => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <div>
+      <section>
+        <div className="mt-[-70]">
+          <Image
+            src={"/images/logo_bringlovehome_b.png"}
+            alt={"Bring Love Home"}
+            // fill
+            className="mx-auto object-cover transition-transform duration-500 ease-out group-hover:scale-105"
+            width={300}
+            height={300}
+          />
+        </div>
         <h2>Sponsor a dog, save a life!</h2>
-      </div>
+      </section>
     </Suspense>
   );
 };
